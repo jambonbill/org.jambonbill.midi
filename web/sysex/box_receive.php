bb91219c6c470ec74e3c9e61d5c5189302dde305
jambonbill <jambonbill@gmail.com>
2016-07-12 16:46:06 +0800

    $box->boxTools

--- web/sysex/box_receive.php
+++ web/sysex/box_receive.php
@@ -4,6 +4,7 @@ $box->title("Receive");
 $box->icon("fa fa-text");
 $box->id("boxReceive");
 $box->collapsable(true);
+$box->boxTools("<button class=\"btn btn-box-tool btnclipboard\" title='Copy to clipboard'><i class='fa fa-copy'></i></button> ");
 $box->body("<textarea class='form-control' id=sysex_in rows=10></textarea>");
 $box->footer("<a href=#save id=saveSysex class='btn btn-default'><i class='fa fa-save'></i> Save</a>");
 echo $box;
