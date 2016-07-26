78706fe54b68166d247961abad47b9abd4c0c221
jambonbill <jambonbill@gmail.com>
2016-07-26 23:59:56 +0800

    patch dump request

--- web/mbsidv2/box_controls.php
+++ web/mbsidv2/box_controls.php
@@ -12,17 +12,22 @@
  0F/a) F0 00 00 7E 4B <device number> 0F F7
         Ping (just sends back the same SysEx string + <sids>)
  */
+$htm="<textarea class='form-control' placeholder='Sysex in' rows=10 id=inputSyx></textarea>";
+$htm.="Midi Output(s)<br />";
+$htm.="<select class='form-control' id=midi_outputs></select>";
 
 $btns=[];
 $btns[]="<a href=# class='btn btn-default' id=btnPing>ping</a> ";
 $btns[]="<a href=# class='btn btn-default' id=btnPlay>play</a> ";
 $btns[]="<a href=# class='btn btn-default' id=btnStop>stop</a> ";
+$btns[]="<a href=# class='btn btn-default' id=btnReq1>patch dump request</a> ";
+$btns[]="<a href=# class='btn btn-default' id=btnReq2>current patch request</a> ";
 
 $box=new LTE\Box;
 $box->title("Controls");
 $box->icon("fa fa-list");
 $box->id("boxControls");
 $box->collapsable(true);
-$box->body("please wait");
+$box->body($htm);
 $box->footer($btns);
 echo $box;
