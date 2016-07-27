ba3fd9badd0e5823773bb8ba46d1a5d10f03183c
jambonbill <jambonbill@gmail.com>
2016-07-27 19:04:36 +0800

    small buttons

--- web/mbsidv2/box_controls.php
+++ web/mbsidv2/box_controls.php
@@ -12,16 +12,17 @@
  0F/a) F0 00 00 7E 4B <device number> 0F F7
         Ping (just sends back the same SysEx string + <sids>)
  */
-$htm="<textarea class='form-control' placeholder='Sysex in' rows=10 id=inputSyx></textarea>";
+$htm='';
+//$htm="<textarea class='form-control' placeholder='Sysex in' rows=10 id=inputSyx></textarea>";
 $htm.="Midi Output(s)<br />";
 $htm.="<select class='form-control' id=midi_outputs></select>";
 
 $btns=[];
 $btns[]="<a href=# class='btn btn-default' id=btnPing>ping</a> ";
-$btns[]="<a href=# class='btn btn-default' id=btnPlay>play</a> ";
-$btns[]="<a href=# class='btn btn-default' id=btnStop>stop</a> ";
-$btns[]="<a href=# class='btn btn-default' id=btnReq1>patch dump request</a> ";
-$btns[]="<a href=# class='btn btn-default' id=btnReq2>current patch request</a> ";
+$btns[]="<a href=# class='btn btn-default' id=btnPlay><i class='fa fa-play'></i></a> ";
+$btns[]="<a href=# class='btn btn-default' id=btnStop><i class='fa fa-stop'></i></a> ";
+$btns[]="<a href=# class='btn btn-default' id=btnReq1>patch dump req</a> ";
+$btns[]="<a href=# class='btn btn-default' id=btnReq2>cur. patch req</a> ";
 
 $box=new LTE\Box;
 $box->title("Controls");
