a5f7aca41eae5e94c7235309304edd6997a6419d
jambonbill <jambonbill@gmail.com>
2016-08-29 15:00:31 +0800

    midi note preview

---
+++ web/jasmid/box_preview.php
@@ -0,0 +1,25 @@
+<?php
+
+$htm="<table class='table table-condensed'>";
+$htm.="<thead>";
+$htm.="<th>Channel</th>";
+$htm.="<th>Playing</th>";
+$htm.="</thead>";
+$htm.="<tbody>";
+
+for($i=0;$i<16;$i++){
+	$htm.="<tr>";	
+	$htm.="<td>".($i+1);
+	$htm.="<td id=voice_$i>-";
+}
+$htm.="</tbody>";
+$htm.="</table>";
+
+
+$box=new LTE\Box;
+$box->title("Preview");
+$box->icon("fa fa-list");
+$box->id("boxPreview");
+$box->collapsable(1);
+$box->body($htm);
+echo $box;
