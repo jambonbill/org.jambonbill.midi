cefe963f98aeaa4111928145685acd16d74eb6a7
jambonbill <jambonbill@gmail.com>
2016-11-08 19:24:09 +0800

    <section class="container">

--- web/home/test.php
+++ web/home/test.php
@@ -9,25 +9,31 @@ require __DIR__."/../../src/MIDI/midi.class.php";
 $admin = new LTE\AdminLte2();
 $admin->title("MIDI");
 echo $admin;
-
-//require('midi.class.php');
-$midi = new MIDI\Midi();
+?>
+<section class="container">
+<?php
+$midi = new Midi();
 
 $files=glob(__DIR__."/../../midifiles/*.mid");
 shuffle($files);
 
 $file=$files[0];
 
-$midi->importMid($file,0);
+$midi->importMid($file);
 $track = $midi->getTrack(0);
+
+echo "<h1>".basename($file)."</h1>\n";
 echo "<pre>";
 
+$tempo=$midi->getTempo();
 $bpm=$midi->getBpm();//returns tempo as beats per minute (0 if tempo not set). 
 $timebase=$midi->getTimebase();//returns timebase value. 
 $trackCount=$midi->getTrackCount();//returns number of tracks. 
+$duration=$midi->getDuration();//in sec
 
-echo basename($file)."\n";
+echo "tempo=$tempo\n";
 echo "BPM=$bpm\n";
+echo "duration=$duration sec\n";
 echo "timebase=$timebase\n";
 echo "trackCount=$trackCount\n";
-print_r($track);
+//print_r($track);
