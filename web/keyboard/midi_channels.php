fa76ad6460f50503ef58b01b5d2dc5e8cbbe694a
orange <jambonbill@gmail.com>
2017-02-05 23:26:39 +0800

    midi channel selector

---
+++ web/keyboard/midi_channels.php
@@ -0,0 +1,31 @@
+
+<div class='row'>
+	<div class='col-sm-12'>
+    <nav aria-label="Midi channel">
+      <ul class="pagination">
+        <li>
+          <a href="#" aria-label="Previous">
+            Midi channel
+          </a>
+        </li>
+        <li class='active'><a href="#" data-channel=0>1</a></li>
+        <li><a href="#" data-channel=1>2</a></li>
+        <li><a href="#" data-channel=2>3</a></li>
+        <li><a href="#" data-channel=3>4</a></li>
+        <li><a href="#" data-channel=4>5</a></li>
+        <li><a href="#" data-channel=5>6</a></li>
+        <li><a href="#" data-channel=6>7</a></li>
+        <li><a href="#" data-channel=7>8</a></li>
+        <li><a href="#" data-channel=8>9</a></li>
+        <li><a href="#" data-channel=9>10</a></li>
+        <li><a href="#" data-channel=10>11</a></li>
+        <li><a href="#" data-channel=11>12</a></li>
+        <li><a href="#" data-channel=12>13</a></li>
+        <li><a href="#" data-channel=13>14</a></li>
+        <li><a href="#" data-channel=14>15</a></li>
+        <li><a href="#" data-channel=15>16</a></li>
+        
+      </ul>
+    </nav>
+</div>
+</div>
