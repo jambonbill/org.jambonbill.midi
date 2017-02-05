a8e6c9fb2dbd18edf675b2dc12644d29c779333d
orange <jambonbill@gmail.com>
2017-02-05 23:26:01 +0800

    $box->collapsed(1);

--- web/keyboard/box_outputs.php
+++ web/keyboard/box_outputs.php
@@ -3,6 +3,6 @@ $box=new LTE\Box;
 $box->title("Output(s)");
 $box->icon("fa fa-plug");
 $box->id("boxOutputs");
-$box->collapsable(true);
+$box->collapsed(1);
 $box->body("<select class='form-control' id=midi_outputs size=2 disabled=disabled></select>");
 echo $box;
