e81df7c8f5a90a652f121d1203079e86a0b2a5f8
orange <jambonbill@gmail.com>
2017-03-30 09:27:30 +0800

    console.log("tada!");

--- web/opafm/js/opafm.js
+++ web/opafm/js/opafm.js
@@ -2,5 +2,9 @@
 // http://www.w3.org/TR/webmidi/#examples-of-web-midi-api-usage-in-javascript
 
 $(function(){
+	$('input').change(function(e){
+		console.info('change',e);
+	});
+
 	console.log("tada!");
 });
