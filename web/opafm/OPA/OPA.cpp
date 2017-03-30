0fdc3bf757c2ac6df8fa6cb0ae31b4b01fd02431
orange <jambonbill@gmail.com>
2017-03-30 09:24:48 +0800

    useful read

---
+++ web/opafm/OPA/OPA.cpp
@@ -0,0 +1,456 @@
+﻿/**
+	fredslab.net : OPA Shield library for Arduino
+
+	The MIT License (MIT)
+
+	Source code copyright (c) 2013-2016 Frédéric Meslin / Thomas Hopper
+	Email: fredericmeslin@hotmail.com
+	Website: www.fredslab.net
+	Twitter: @marzacdev
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+	
+	Version 1.0, 30/07/2016
+*/
+
+#include "OPA.h"
+#include "Arduino.h"
+
+/*****************************************************************************/
+#define OPA_CS1_STATE(a) (a & 0x01)
+#define OPA_CS2_STATE(a) ((a >> 1) & 0x01)
+
+/*****************************************************************************/
+#if defined ARDUINO_LEONARDO
+	#define SerialPort Serial1
+#elif defined ARDUINO_MEGA
+	#define SerialPort Serial1
+#elif defined ARDUINO_UNO
+	#define SerialPort Serial
+#elif defined ARDUINO_OTHER
+	#define SerialPort Serial
+#else
+	#error "No Arduino board has been defined, please configure the opa.h file!"
+	#define SerialPort Serial
+#endif
+
+/*****************************************************************************/
+OPA::OPA() :
+	address(OPA_ADDRESS_0),
+	error(OPA_ERROR_NONE)
+{
+	initPins();
+}
+
+OPA::OPA(OPA_ADDRESSES address) :
+	address(address),
+	error(OPA_ERROR_NONE)
+{
+	initPins();
+}
+
+/*****************************************************************************/
+void OPA::initPins()
+{
+	pinMode(OPA_TX_PIN, INPUT);
+	pinMode(OPA_RX_PIN, INPUT);
+	pinMode(OPA_CS1_PIN, OUTPUT);
+	pinMode(OPA_CS2_PIN, OUTPUT);
+	pinMode(OPA_SWAP_PIN, OUTPUT);
+	pinMode(OPA_RESET_PIN, OUTPUT);
+
+	digitalWrite(OPA_CS1_PIN, 1);
+	digitalWrite(OPA_CS2_PIN, 1);
+	digitalWrite(OPA_SWAP_PIN, 0);
+	digitalWrite(OPA_RESET_PIN, 0);
+}
+
+/*****************************************************************************/
+void OPA::enable()
+{
+	digitalWrite(OPA_CS1_PIN, OPA_CS1_STATE(address));
+	digitalWrite(OPA_CS2_PIN, OPA_CS2_STATE(address));
+	SerialPort.begin(OPA_BAUDRATE);
+	SerialPort.setTimeout(OPA_SERIAL_TIMEOUT);
+	clearErrors();
+}
+
+void OPA::disable()
+{
+	SerialPort.end();
+	digitalWrite(OPA_CS1_PIN, 1);
+	digitalWrite(OPA_CS2_PIN, 1);
+}
+
+/*****************************************************************************/
+void OPA::reset()
+{
+	digitalWrite(OPA_RESET_PIN, 1);
+	delay(2);
+	digitalWrite(OPA_RESET_PIN, 0);
+	delay(2);
+	error = OPA_ERROR_NONE;
+}
+
+/*****************************************************************************/
+char * OPA::readVersion()
+{
+	static char version[24];
+	char buffer[1];
+	buffer[0] = OPA_CODE_VERSION;
+	SerialPort.write(buffer, 1);
+	if (SerialPort.readBytes(version, 24) == 24)
+		return version;
+	error = OPA_ERROR_TIMEOUT;
+	return 0;
+}
+
+/*****************************************************************************/
+void OPA::noteOn(OPA_PROGRAMS program, uint8_t note, uint8_t fraction, uint8_t nuance)
+{
+	char buffer[5];
+	buffer[0] = OPA_CODE_NOTEON;
+	buffer[1] = program;
+	buffer[2] = note;
+	buffer[3] = fraction;
+	buffer[4] = nuance;
+	SerialPort.write(buffer, 5);
+}
+
+void OPA::noteOff(OPA_PROGRAMS program, uint8_t note, uint8_t fraction, uint8_t nuance)
+{
+	char buffer[5];
+	buffer[0] = OPA_CODE_NOTEOFF;
+	buffer[1] = program;
+	buffer[2] = note;
+	buffer[3] = fraction;
+	buffer[4] = nuance;
+	SerialPort.write(buffer, 5);
+}
+
+void OPA::allNotesOff(OPA_PROGRAMS program)
+{
+	char buffer[2];
+	buffer[0] = OPA_CODE_ALLNOTESOFF;
+	buffer[1] = program;
+	SerialPort.write(buffer, 2);
+}
+
+void OPA::allSoundsOff()
+{
+	char buffer[2];
+	buffer[0] = OPA_CODE_ALLSOUNDSOFF;
+	SerialPort.write(buffer, 1);
+}
+
+/*****************************************************************************/
+void OPA::pitchBend(OPA_PROGRAMS program, int8_t coarse, int8_t fine)
+{
+	char buffer[4];
+	buffer[0] = OPA_CODE_PITCHBEND;
+	buffer[1] = program;
+	buffer[2] = coarse;
+	buffer[3] = fine;
+	SerialPort.write(buffer, 4);
+}
+
+/*****************************************************************************/
+void OPA::setMemoryProtection(bool protection)
+{
+	int flags = readGlobalParam(OPA_GLOBAL_FLAGS);
+	flags &= ~OPA_GLOBAL_PROTECT;
+	if (protection) flags |= OPA_GLOBAL_PROTECT;
+	writeGlobalParam(OPA_GLOBAL_FLAGS, flags);
+}
+
+/*****************************************************************************/
+void OPA::writeOperatorParam(OPA_PROGRAMS program, OPA_OPERATORS op, OPA_OP_PARAMETERS param, uint8_t value)
+{
+	uint8_t p = op * OPA_OP_PARAMS_NB + OPA_PROGS_PARAMS_NB + param;
+	writeFMParam(program, p, value);
+}
+
+uint8_t OPA::readOperatorParam(OPA_PROGRAMS program, OPA_OPERATORS op, OPA_OP_PARAMETERS param)
+{
+	uint8_t p = op * OPA_OP_PARAMS_NB + OPA_PROGS_PARAMS_NB + param;
+	return readFMParam(program, p);
+}
+
+/*****************************************************************************/
+void OPA::writeGlobalParam(OPA_GLOBAL_PARAMETERS param, uint8_t value)
+{
+/** Send a parameter */
+	char buffer[3];
+	buffer[0] = OPA_CODE_GLOBALSPARAMWRITE;
+	buffer[1] = param;
+	buffer[2] = value;
+	SerialPort.write(buffer, 6);
+}
+
+void OPA::writeFMParam(OPA_PROGRAMS program, uint8_t param, uint8_t value)
+{
+/** Send a parameter */
+	char buffer[4];
+	buffer[0] = OPA_CODE_FMPARAMWRITE;
+	buffer[1] = program;
+	buffer[2] = param;
+	buffer[3] = value;
+	SerialPort.write(buffer, 4);
+}
+
+uint8_t OPA::writeKitParam(int sample, OPA_KIT_PARAMETERS param, uint8_t value)
+{
+/** Send a parameter */
+	char buffer[4];
+	buffer[0] = OPA_CODE_KITPARAMWRITE;
+	buffer[1] = sample;
+	buffer[2] = param;
+	buffer[3] = value;
+	SerialPort.write(buffer, 4);
+}
+
+uint8_t OPA::readGlobalParam(OPA_GLOBAL_PARAMETERS param)
+{
+/** Send a parameter request */
+	char buffer[3];
+	buffer[0] = OPA_CODE_GLOBALSPARAMREAD;
+	buffer[1] = param;
+	buffer[2] = 0;
+	SerialPort.write(buffer, 3);
+
+/** Check the reply */
+	if (SerialPort.readBytes(buffer, 3) == 3) {
+		if (buffer[0] == OPA_CODE_GLOBALSPARAMWRITE)
+			return buffer[2];
+		error = OPA_ERROR_BADREPLY;
+		return 0;
+	}
+	error = OPA_ERROR_TIMEOUT;
+	return 0;
+}
+
+uint8_t OPA::readFMParam(OPA_PROGRAMS program, uint8_t param)
+{
+/** Send a parameter request */
+	char buffer[4];
+	buffer[0] = OPA_CODE_FMPARAMREAD;
+	buffer[1] = program;
+	buffer[2] = param;
+	buffer[3] = 0;
+	SerialPort.write(buffer, 4);
+
+/** Check the reply */
+	if (SerialPort.readBytes(buffer, 4) == 4) {
+		if (buffer[0] == OPA_CODE_FMPARAMWRITE)
+			return buffer[3];
+		error = OPA_ERROR_BADREPLY;
+		return 0;
+	}
+	error = OPA_ERROR_TIMEOUT;
+	return 0;
+}
+
+uint8_t OPA::readKitParam(int sample, OPA_KIT_PARAMETERS param)
+{
+/** Send a parameter request */
+	char buffer[4];
+	buffer[0] = OPA_CODE_KITPARAMREAD;
+	buffer[1] = sample;
+	buffer[2] = param;
+	buffer[3] = 0;
+	SerialPort.write(buffer, 4);
+
+/** Check the reply */
+	if (SerialPort.readBytes(buffer, 4) == 4) {
+		if (buffer[0] == OPA_CODE_KITPARAMWRITE)
+			return buffer[3];
+		error = OPA_ERROR_BADREPLY;
+		return 0;
+	}
+	error = OPA_ERROR_TIMEOUT;
+	return 0;
+}
+
+
+/*****************************************************************************/
+void OPA::writeProgram(OPA_PROGRAMS program, OpaProgram &programData)
+{
+    char buffer[4];
+	if (program >= OPA_PROGS_NB) {
+		error = OPA_ERROR_BADPARAMETER;
+		return;
+	}
+    buffer[0] = OPA_CODE_PROGRAMWRITE;
+    buffer[1] = program;
+    buffer[2] = sizeof(OpaProgram);
+    buffer[3] = 0;
+    SerialPort.write(buffer, 4);
+    SerialPort.write((char *) &programData, sizeof(OpaProgram));
+}
+
+void OPA::readProgram(OPA_PROGRAMS program, OpaProgram &programData)
+{
+    char buffer[4];
+	if (program >= OPA_PROGS_NB) {
+		error = OPA_ERROR_BADPARAMETER;
+		return;
+	}
+
+/** Clear the program */
+	memset(&programData, 0, sizeof(OpaProgram));
+
+/** Send a program request */
+    buffer[0] = OPA_CODE_PROGRAMREAD;
+    buffer[1] = program;
+    buffer[2] = sizeof(OpaProgram);
+    buffer[3] = 0;
+	SerialPort.write(buffer, 4);
+
+/** Check the reply */
+	if (SerialPort.readBytes(buffer, 4) != 4) {
+		error = OPA_ERROR_TIMEOUT;
+		return;
+	}
+	if (buffer[0] != OPA_CODE_PROGRAMWRITE) {
+		error = OPA_ERROR_BADREPLY;
+		return;
+	}
+	char * data = (char *) &programData;
+	if (SerialPort.readBytes(data, sizeof(OpaProgram)) != sizeof(OpaProgram)) {
+		error = OPA_ERROR_TIMEOUT;
+		return;
+	}
+}
+
+
+/*****************************************************************************/
+void OPA::writeKit(OpaKit &kitData)
+{
+    char buffer[3];
+    buffer[0] = OPA_CODE_KITWRITE;
+    buffer[1] = sizeof(OpaKit);
+    buffer[2] = 0;
+	SerialPort.write(buffer, 3);
+	SerialPort.write((char *) &kitData, sizeof(OpaKit));
+}
+
+void OPA::readKit(OpaKit &kitData)
+{
+/** Clear the kit */
+    char buffer[3];
+	memset(&kitData, 0, sizeof(OpaKit));
+
+/** Send a program request */
+    buffer[0] = OPA_CODE_KITREAD;
+    buffer[1] = sizeof(OpaKit);
+	buffer[2] = 0;
+	SerialPort.write(buffer, 3);
+
+/** Check the reply */
+	if (SerialPort.readBytes(buffer, 3) != 3) {
+		error = OPA_ERROR_TIMEOUT;
+		return;
+	}
+	if (buffer[0] != OPA_CODE_KITWRITE) {
+		error = OPA_ERROR_BADREPLY;
+		return;
+	}
+	char * data = (char *) &kitData;
+	if (SerialPort.readBytes(data, sizeof(OpaKit)) != sizeof(OpaKit)) {
+		error = OPA_ERROR_TIMEOUT;
+		return;
+	}
+}
+
+/*****************************************************************************/
+void OPA::storeInternal(OPA_PROGRAMS program, uint8_t slot)
+{
+	char buffer[3];
+	if (slot >= OPA_MAX_SLOTS) {
+		error = OPA_ERROR_BADPARAMETER;
+		return;
+	}
+	buffer[0] = OPA_CODE_INTERNALSTORE;
+	buffer[1] = program;
+	buffer[2] = slot;
+	SerialPort.write(buffer, 3);
+}
+
+void OPA::loadInternal(OPA_PROGRAMS program, uint8_t slot)
+{
+	char buffer[3];
+	if (slot >= OPA_MAX_SLOTS) {
+		error = OPA_ERROR_BADPARAMETER;
+		return;
+	}
+	buffer[0] = OPA_CODE_INTERNALLOAD;
+	buffer[1] = program;
+	buffer[2] = slot;
+	SerialPort.write(buffer, 3);
+}
+
+/*****************************************************************************/
+void OPA::writeInternal(uint8_t slot, OpaProgram &programData)
+{
+    char buffer[4];
+	if (slot >= OPA_MAX_SLOTS) {
+		error = OPA_ERROR_BADPARAMETER;
+		return;
+	}
+    buffer[0] = OPA_CODE_INTERNALWRITE;
+    buffer[1] = slot;
+    buffer[2] = sizeof(OpaProgram);
+    buffer[3] = 0;
+    SerialPort.write(buffer, 4);
+    SerialPort.write((char *) &programData, sizeof(OpaProgram));
+}
+
+void OPA::readInternal(uint8_t slot, OpaProgram &programData)
+{
+    char buffer[4];
+	if (slot >= OPA_MAX_SLOTS) {
+		error = OPA_ERROR_BADPARAMETER;
+		return;
+	}
+
+/** Clear the program */
+	memset(&programData, 0, sizeof(OpaProgram));
+
+/** Send a program request */
+    buffer[0] = OPA_CODE_INTERNALREAD;
+    buffer[1] = slot;
+    buffer[2] = sizeof(OpaProgram);
+    buffer[3] = 0;
+	SerialPort.write(buffer, 4);
+
+/** Check the reply */
+	if (SerialPort.readBytes(buffer, 4) != 4) {
+		error = OPA_ERROR_TIMEOUT;
+		return;
+	}
+	if (buffer[0] != OPA_CODE_INTERNALWRITE) {
+		error = OPA_ERROR_BADREPLY;
+		return;
+	}
+	char * data = (char *) &programData;
+	if (SerialPort.readBytes(data, sizeof(OpaProgram)) != sizeof(OpaProgram)) {
+		error = OPA_ERROR_TIMEOUT;
+		return;
+	}
+}
---
+++ web/opafm/OPA/OPA.h
@@ -0,0 +1,323 @@
+﻿/**
+	fredslab.net : OPA Shield library for Arduino
+
+	The MIT License (MIT)
+
+	Source code copyright (c) 2013-2016 Frédéric Meslin / Thomas Hopper
+	Email: fredericmeslin@hotmail.com
+	Website: www.fredslab.net
+	Twitter: @marzacdev
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+	
+	Version 1.0, 30/07/2016
+*/
+
+#ifndef OPA_H
+#define OPA_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+/*****************************************************************************/
+/**
+  IMPORTANT: Board selection
+  The type of Arduino board must be defined so the OPA library
+  can communicate with the shield.
+
+  Please define here either:
+  ARDUINO_LEONARDO
+  ARDUINO_MEGA
+  ARDUINO_UNO
+  ARDUINO_OTHER
+  
+**/
+
+#define ARDUINO_LEONARDO
+//#define ARDUINO_MEGA
+//#define ARDUINO_UNO
+//#define ARDUINO_OTHER
+  
+ /*****************************************************************************/
+/** OPA shield configuration */
+	const unsigned int OPA_BAUDRATE	= 57600;	/** Communication baudrate in bauds */
+	const int OPA_SERIAL_TIMEOUT	= 500;		/** Communication timeout when reading */
+
+/** OPA chip properties */
+	const int OPA_MAX_SLOTS 		= 104;		/** Number of program slots in internal memory */
+
+	const int OPA_PROGS_NB			= 8;		/** Number of programs in RAM */
+	const int OPA_GLOBAL_PARAMS_NB	= 8;		/** Number of parameters in global configuration */
+	const int OPA_PROGS_PARAMS_NB	= 12;		/** Number of parameters in each program */
+	const int OPA_OP_PARAMS_NB		= 16;		/** Number of parameters in each operator */
+	const int OPA_KIT_SAMPLES_NB	= 32;		/** Number of samples in kit */
+
+	const int OPA_ALLPROGS_ID		= 255;
+
+/** OPA shield pin-mapping */
+	const int OPA_TX_PIN 			= 0;
+	const int OPA_RX_PIN 			= 1;
+	const int OPA_CS1_PIN 			= 2;
+	const int OPA_CS2_PIN 			= 3;
+	const int OPA_SWAP_PIN 			= 4;
+	const int OPA_RESET_PIN 		= 7;
+
+/*****************************************************************************/
+	typedef enum{
+		OPA_CODE_VERSION			= 0,
+		OPA_CODE_NOTEON				= 1,
+		OPA_CODE_NOTEOFF			= 2,
+		OPA_CODE_ALLNOTESOFF		= 3,
+		OPA_CODE_ALLSOUNDSOFF		= 4,
+		OPA_CODE_FMPARAMWRITE		= 5,
+		OPA_CODE_FMPARAMREAD		= 6,
+		OPA_CODE_GLOBALSPARAMWRITE	= 7,
+		OPA_CODE_GLOBALSPARAMREAD	= 8,
+		OPA_CODE_PROGRAMWRITE		= 9,
+		OPA_CODE_PROGRAMREAD		= 10,
+		OPA_CODE_GLOBALSWRITE		= 11,
+		OPA_CODE_GLOBALSREAD		= 12,
+		OPA_CODE_INTERNALSTORE		= 13,
+		OPA_CODE_INTERNALLOAD		= 14,
+		OPA_CODE_INTERNALWRITE		= 15,
+		OPA_CODE_INTERNALREAD		= 16,
+		OPA_CODE_PITCHBEND			= 17,
+		OPA_CODE_KITPARAMWRITE		= 18,
+		OPA_CODE_KITPARAMREAD		= 19,
+		OPA_CODE_KITWRITE			= 20,
+		OPA_CODE_KITREAD			= 21,
+	}OPA_CODE_MESSAGES;
+
+
+/*****************************************************************************/
+	typedef enum{
+		OPA_GLOBAL_PROTECT		= 1,
+		OPA_GLOBAL_MUTEFM		= 2,
+		OPA_GLOBAL_MUTEKIT		= 4,
+		OPA_GLOBAL_DEFAULT		= OPA_GLOBAL_PROTECT,
+	}OPA_GLOBAL_FLAGBITS;
+
+	typedef enum{
+		OPA_PROGRAM_STEALING	= 1,
+		OPA_PROGRAM_MUTED		= 2,
+		OPA_PROGRAM_DEFAULT		= OPA_PROGRAM_STEALING,
+	}OPA_PROGRAM_FLAGBITS;
+
+	typedef enum{
+		OPA_OP_ABSOLUTE			= 1,
+		OPA_OP_SOFT_LOW			= 2,
+		OPA_OP_HARD_LOW			= 4,
+		OPA_OP_SOFT_HIGH		= 8,
+		OPA_OP_HARD_HIGH		= 16,
+		OPA_OP_MUTED			= 32,
+		OPA_OP_DEFAULT			= 0,
+	}OPA_OP_FLAGBITS;
+
+/*****************************************************************************/
+	typedef struct{
+		uint8_t volume;
+		int8_t  coarse;
+		int8_t  fine;
+		uint8_t flags;
+		uint8_t fmVolume;
+		uint8_t kitVolume;
+		uint8_t reserved1;
+		uint8_t reserved2;
+	}OpaGlobals;
+
+/*****************************************************************************/
+	typedef struct{
+		uint8_t name[8];
+		uint8_t algorithm;
+		uint8_t volume;
+		uint8_t panning;
+		uint8_t flags;
+	}OpaProgramParams;
+
+	typedef struct{
+		uint8_t volume;
+		uint8_t coarse;
+		int8_t  fine;
+		uint8_t envAttack;
+		uint8_t envDecay;
+		uint8_t envSusLevel;
+		uint8_t envIniLevel;
+		uint8_t envRelease;
+		uint8_t LFOSpeed;
+		uint8_t LFOAmount;
+		uint8_t feedback;
+		uint8_t flags;
+		uint8_t reserved1;
+		uint8_t reserved2;
+		uint8_t reserved3;
+		uint8_t reserved4;
+	}OpaOperatorParams;
+
+	typedef struct{
+		OpaProgramParams params;
+		OpaOperatorParams opParams[4];
+	}OpaProgram;
+
+/*****************************************************************************/
+	typedef struct{
+		uint8_t volume;
+		uint8_t panning;
+		uint8_t decay;
+		uint8_t reserved;
+	}OpaKitParams;
+
+	typedef struct{
+		OpaKitParams params[OPA_KIT_SAMPLES_NB];
+	}OpaKit;
+
+/*****************************************************************************/
+	typedef enum{
+		OPA_ADDRESS_0			= 0,
+		OPA_ADDRESS_1			= 1,
+		OPA_ADDRESS_2			= 2,
+	} OPA_ADDRESSES;
+
+	typedef enum{
+		OPA_PROGRAM_0			= 0,
+		OPA_PROGRAM_1			= 1,
+		OPA_PROGRAM_2			= 2,
+		OPA_PROGRAM_3			= 3,
+		OPA_PROGRAM_4			= 4,
+		OPA_PROGRAM_5			= 5,
+		OPA_PROGRAM_6			= 6,
+		OPA_PROGRAM_7			= 7,
+		OPA_PROGRAM_DRUMS		= 9,
+		OPA_PROGRAM_ALL			= 255,
+	} OPA_PROGRAMS;
+
+	typedef enum{
+		OPA_OPERATOR_0			= 0,
+		OPA_OPERATOR_1			= 1,
+		OPA_OPERATOR_2			= 2,
+		OPA_OPERATOR_3			= 3,
+	} OPA_OPERATORS;
+
+	typedef enum{
+		OPA_ERROR_NONE			= 0x00,
+		OPA_ERROR_TIMEOUT		= 0x01,
+		OPA_ERROR_BADREPLY		= 0x02,
+		OPA_ERROR_BADPARAMETER	= 0x10,
+	} OPA_ERRORS;
+
+/*****************************************************************************/
+	typedef enum{
+		OPA_GLOBAL_VOLUME		= 0,
+		OPA_GLOBAL_COARSE		= 1,
+		OPA_GLOBAL_FINE			= 2,
+		OPA_GLOBAL_FLAGS		= 3,
+	} OPA_GLOBAL_PARAMETERS;
+
+/*****************************************************************************/
+	typedef enum{
+		OPA_PROGRAM_NAME		= 0,
+		OPA_PROGRAM_ALGORITHM	= 8,
+		OPA_PROGRAM_VOLUME		= 9,
+		OPA_PROGRAM_PANNING		= 10,
+		OPA_PROGRAM_FLAGS		= 11,
+	} OPA_PROGRAM_PARAMETERS;
+
+	typedef enum{
+		OPA_OP_VOLUME			= 0,
+		OPA_OP_COARSE			= 1,
+		OPA_OP_FINE				= 2,
+		OPA_OP_ENVATTACK		= 3,
+		OPA_OP_ENVDECAY			= 4,
+		OPA_OP_ENVSUSTAINLEVEL	= 5,
+		OPA_OP_ENVINITLEVEL		= 6,
+		OPA_OP_ENVRELEASE		= 7,
+		OPA_OP_LFOSPEED			= 8,
+		OPA_OP_LFOAMOUNT		= 9,
+		OPA_OP_FEEDBACK			= 10,
+		OPA_OP_FLAGS			= 11,
+	} OPA_OP_PARAMETERS;
+
+	typedef enum{
+		OPA_SAMPLE_VOLUME		= 0,
+		OPA_SAMPLE_PANNING		= 1,
+		OPA_SAMPLE_DECAY		= 2,
+		OPA_SAMPLE_RESERVED		= 3,
+	}OPA_KIT_PARAMETERS;
+
+/*****************************************************************************/
+class OPA{
+public:
+/** OPA class constructor **/
+	OPA();
+	OPA(OPA_ADDRESSES address);
+
+	void enable();
+	void disable();
+	void reset();
+
+/** Version / protection **/
+	char * readVersion();
+	void setMemoryProtection(bool protection);
+
+/** Playing notes **/
+	void noteOn(OPA_PROGRAMS program, uint8_t note, uint8_t fraction = 0, uint8_t nuance = 255);
+	void noteOff(OPA_PROGRAMS program, uint8_t note, uint8_t fraction = 0, uint8_t nuance = 255);
+	void pitchBend(OPA_PROGRAMS program, int8_t coarse, int8_t fine);
+	void allNotesOff(OPA_PROGRAMS program);
+	void allSoundsOff();
+
+/** Reading & writing parameter helpers **/
+	void writeGlobalParam(OPA_GLOBAL_PARAMETERS param, uint8_t value);
+	void writeFMParam(OPA_PROGRAMS program, uint8_t param, uint8_t value);
+	void writeOperatorParam(OPA_PROGRAMS program, OPA_OPERATORS op, OPA_OP_PARAMETERS param, uint8_t value);
+	uint8_t writeKitParam(int sample, OPA_KIT_PARAMETERS param, uint8_t value);
+
+	uint8_t readGlobalParam(OPA_GLOBAL_PARAMETERS param);
+	uint8_t readFMParam(OPA_PROGRAMS program, uint8_t param);
+	uint8_t readOperatorParam(OPA_PROGRAMS program, OPA_OPERATORS op, OPA_OP_PARAMETERS param);
+	uint8_t readKitParam(int sample, OPA_KIT_PARAMETERS param);
+
+/** Reading & writing full programs **/
+	void writeGlobals(OpaGlobals &globalsData);
+	void readGlobals(OpaGlobals &globalsData);
+	void writeProgram(OPA_PROGRAMS program, OpaProgram &programData);
+	void readProgram(OPA_PROGRAMS program, OpaProgram &programData);
+	void writeKit(OpaKit &kitData);
+	void readKit(OpaKit &kitData);
+
+/** Store, load, write and read programs from internal memory **/
+	void storeInternal(OPA_PROGRAMS program, uint8_t slot);
+	void loadInternal(OPA_PROGRAMS program, uint8_t slot);
+	void writeInternal(uint8_t slot, OpaProgram &programData);
+	void readInternal(uint8_t slot, OpaProgram &programData);
+
+/** Error handling **/
+	void clearErrors()
+		{error = OPA_ERROR_NONE;}
+
+	OPA_ERRORS getErrors()
+		{return error;}
+
+private:
+	OPA_ADDRESSES address;
+	OPA_ERRORS error;
+
+	void initPins();
+};
+
+#endif
