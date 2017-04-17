25482230e3fe4155fc526227fa2da1ac58059b91
orange <jambonbill@gmail.com>
2017-04-17 21:52:16 +0800

    cleaned

--- web/keyboard/js/keyboard.js
+++ web/keyboard/js/keyboard.js
@@ -1,31 +1,6 @@
 // jambonbill midi.js  - keyboard
 // http://www.w3.org/TR/webmidi/#examples-of-web-midi-api-usage-in-javascript
 
-function displayKeyMap()
-{
-    console.log('displayKeyMap()');
-    var htm=[];
-    htm.push('<table class="table table-condensed">');
-    htm.push('<thead>');
-    htm.push('<th>Key</th>');
-    htm.push('<th>Note</th>');
-    htm.push('<th>Value</th>');
-    htm.push('</thead>');
-    htm.push('<tbody>');
-    for (var i=0;i<kmap.length;i++) {
-        if(!kmap[i])continue;
-        var n=keyCodeToMidiNote(i);
-        htm.push("<tr>")
-        htm.push("<td>"+i);
-        htm.push("<td>"+notestr[n]);
-        htm.push("<td>"+n);
-    }
-    htm.push('</tbody>');
-    htm.push('</table>');
-    $('#boxLog .box-body').html(htm.join(''));
-}
-
-
 $(function(){
 	
 	//var context=null;   // the Web Audio "context" object
@@ -37,49 +12,31 @@ $(function(){
 	var _prg=0;//current prg
 	var _notes=[];// note buffer (being played)
 	var _notestr=['C-','C#','D-','D#','E-','F-','F#','G-','G#','A-','A#','B-'];
-	var kmap=[];// Keyboard mapping
-	// QWERTY //
-	kmap[90]='0';// Z - Octave 1
-	kmap[83]=1;// S
-	kmap[88]=2;// X
-	kmap[68]=3;// D
-	kmap[67]=4;// C
-	kmap[86]=5;// V
-	kmap[71]=6;// G
-	kmap[66]=7;// B
-	kmap[72]=8;// H
-	kmap[78]=9;// N
-	kmap[74]=10;// J
-	kmap[77]=11;// M
-	kmap[188]=12;// <
-	kmap[190]=14;// <
 	
-	kmap[81]=12;// Q - Octave 2
-	kmap[50]=13;// 2 - Octave 2
-	kmap[87]=14;// W - Octave 2
-	kmap[51]=15;// 3 - Octave 2
-	kmap[69]=16;// E - Octave 2
-	kmap[82]=17;// R - Octave 2
-	kmap[53]=18;// 5 - Octave 2
-	kmap[84]=19;// T - Octave 2
-	kmap[54]=20;// 6 - Octave 2
-	kmap[89]=21;// Y - Octave 2
-	kmap[55]=22;// 7 - Octave 2
-	kmap[85]=23;// U - Octave 2
-	kmap[73]=24;// I - Octave 3
-	kmap[57]=25;// 9 - Octave 3
-	kmap[79]=26;// O - Octave 3
-	kmap[48]=27;// 0 - Octave 3
-	kmap[80]=28;// P - Octave 3
-	kmap[219]=29;//
-	kmap[221]=31;//
-	
-	function keyCodeToMidiNote(keyCode)
-	{
-		var n=kmap[keyCode];
-		if (n) return +n;	
-	}
 
+	function displayKeyMap()
+	{
+	    console.log('displayKeyMap()');
+	    var htm=[];
+	    htm.push('<table class="table table-condensed">');
+	    htm.push('<thead>');
+	    htm.push('<th>Key</th>');
+	    htm.push('<th>Note</th>');
+	    htm.push('<th>Value</th>');
+	    htm.push('</thead>');
+	    htm.push('<tbody>');
+	    for (var i=0;i<kmap.length;i++) {
+	        if(!kmap[i])continue;
+	        var n=keyCodeToMidiNote(i);
+	        htm.push("<tr>")
+	        htm.push("<td>"+i);
+	        htm.push("<td>"+notestr[n]);
+	        htm.push("<td>"+n);
+	    }
+	    htm.push('</tbody>');
+	    htm.push('</table>');
+	    $('#boxLog .box-body').html(htm.join(''));
+	}
 
 
 	function setPortId(id){
