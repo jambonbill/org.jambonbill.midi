8a930a987aef6ec027d6b8eaffc567c959a6d8da
orange <jambonbill@gmail.com>
2017-04-23 22:16:32 +0800

    $json_errors = array();

--- web/control_changes/ctrl.php
+++ web/control_changes/ctrl.php
@@ -5,6 +5,14 @@ session_start();
 
 require __DIR__."/../../vendor/autoload.php";
 
+$constants = get_defined_constants(true);
+$json_errors = array();
+foreach ($constants["json"] as $name => $value) {
+    if (!strncmp($name, "JSON_ERROR_", 11)) {
+        $json_errors[$value] = $name;
+    }
+}
+
 $dat=[];
 switch($_POST['do']){
 	
@@ -25,7 +33,15 @@ switch($_POST['do']){
 		$dat['POST']=$_POST;
 		$configfile="configs/".$_POST['filename'];
 		if(is_file($configfile)){
-			$config=json_decode(file_get_contents($configfile));	
+			$raw=file_get_contents($configfile);
+			$dat['raw']=$raw;
+
+			$config=json_decode($raw);	
+			
+			if(json_last_error()){
+				$dat['error']=$json_errors[json_last_error()];	
+			}
+			
 			$dat['config']=$config;
 		}else{
 			$dat['error']="file not found";
