7571542fadcc0ec05aa4e5803c579afb6d6eaf12
orange <jambonbill@gmail.com>
2017-04-27 00:11:21 +0800

    value=120 max=127 name=Volume

--- web/opafm/box_operator.php
+++ web/opafm/box_operator.php
@@ -23,14 +23,14 @@ $htm.='<div class=row>';
 $htm.='<div class=col-xs-3>';
 $htm.='<div class="form-group">';
 $htm.='<label>Volume</label>';
-$htm.='<input type=range data-cc='.($OP*16+0).' value=120 max=127 name=volume>';
+$htm.='<input type=range data-cc='.($OP*16+0).' value=120 max=127 name=Volume>';
 $htm.='</div></div>';
 
 
 $htm.='<div class=col-xs-3>';
 $htm.='<div class="form-group">';
 $htm.='<label>Coarse</label>';
-$htm.='<input type=range data-cc='.($OP*16+1).' value=12 max=127 name=coarse>';
+$htm.='<input type=range data-cc='.($OP*16+1).' value=12 max=127 name=Coarse>';
 $htm.='</div></div>';
 /*
 $htm.='<div class=col-sm-3>';
@@ -43,57 +43,57 @@ $htm.='</div></div>';
 $htm.='<div class=col-xs-3>';
 $htm.='<div class="form-group">';
 $htm.='<label>IniLevel</label>';
-$htm.='<input type=range data-cc='.($OP*16+3).' value=0 max=127 name=envIniLevel>';
+$htm.='<input type=range data-cc='.($OP*16+3).' value=0 max=127 name=IniLevel>';
 $htm.='</div></div>';
 
 
 $htm.='<div class=col-xs-3>';
 $htm.='<div class="form-group">';
 $htm.='<label>Attack</label>';
-$htm.='<input type=range data-cc='.($OP*16+4).' value=0 max=127 name=envAttack>';
+$htm.='<input type=range data-cc='.($OP*16+4).' value=0 max=127 name=Attack>';
 $htm.='</div></div>';
 
 $htm.='<div class=col-xs-3>';
 $htm.='<div class="form-group">';
 $htm.='<label>Decay</label>';
-$htm.='<input type=range data-cc='.($OP*16+5).' value=0 max=127 name=envDecay>';
+$htm.='<input type=range data-cc='.($OP*16+5).' value=0 max=127 name=Decay>';
 $htm.='</div></div>';
 
 $htm.='<div class=col-xs-3>';
 $htm.='<div class="form-group">';
 $htm.='<label>Sustain</label>';
-$htm.='<input type=range data-cc='.($OP*16+6).' value=0 max=127 name=envSusLevel>';
+$htm.='<input type=range data-cc='.($OP*16+6).' value=0 max=127 name=Sustain>';
 $htm.='</div></div>';
 
 $htm.='<div class=col-xs-3>';
 $htm.='<div class="form-group">';
 $htm.='<label>Release</label>';
-$htm.='<input type=range data-cc='.($OP*16+7).' value=0 max=127 name=envRelease>';
+$htm.='<input type=range data-cc='.($OP*16+7).' value=0 max=127 name=Release>';
 $htm.='</div></div>';
 
 $htm.='<div class=col-xs-3>';
 $htm.='<div class="form-group">';
 $htm.='<label>LFO Speed</label>';
-$htm.='<input type=range data-cc='.($OP*16+8).' value=0 max=127 name=LFOSpeed>';
+$htm.='<input type=range data-cc='.($OP*16+8).' value=0 max=127 name="LFOSpd">';
 $htm.='</div></div>';
 
 $htm.='<div class=col-xs-3>';
 $htm.='<div class="form-group">';
 $htm.='<label>LFO Amount</label>';
-$htm.='<input type=range data-cc='.($OP*16+9).' value=0 max=127 name=LFOAmount>';
+$htm.='<input type=range data-cc='.($OP*16+9).' value=0 max=127 name="LFOAmnt">';
 $htm.='</div></div>';
 
 if ($OP==4) {
     $htm.='<div class=col-xs-3>';
     $htm.='<div class="form-group">';
     $htm.='<label>Feedback</label>';
-    $htm.='<input type=range data-cc='.($OP*16+10).' value=0 max=127 name=feedback>';
+    $htm.='<input type=range data-cc='.($OP*16+10).' value=0 max=127 name=Feedback>';
     $htm.='</div></div>';
 
     $htm.='<div class=col-xs-3>';
     $htm.='<div class="form-group">';
     $htm.='<label>Flags</label>';
-    $htm.='<input type=range data-cc='.($OP*16+11).' value=0 max=127 name=flags>';
+    $htm.='<input type=range data-cc='.($OP*16+11).' value=0 max=127 name=Flags>';
     $htm.='</div></div>';
 }
 
