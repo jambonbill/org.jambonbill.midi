bed0fabd72eb968d2d4645bd9a90e19ccb661add
jambonbill <jambonbill@gmail.com>
2017-04-30 15:34:49 +0800

    load Internal

--- web/opafm/box_actions.php
+++ web/opafm/box_actions.php
@@ -3,7 +3,10 @@
 $htm='<div class="row">';
 $htm.='<div class="col-md-12">';
 $htm.='<a href=#btn id=btnRandom class="btn btn-default">Random patch</a> ';
-$htm.='<a href=#btn id=btnKill class="btn btn-default">Kill all notes</a> ';
+$htm.='<a href=#btn id=btnLoad class="btn btn-default">load Internal</a> ';
+$htm.='<a href=#btn id=btnStore class="btn btn-default">store Internal</a> ';
+$htm.='<a href=#btn id=btnKill1 class="btn btn-default">all notes off</a> ';
+$htm.='<a href=#btn id=btnKill2 class="btn btn-default">all sounds off</a> ';
 $htm.='<a href=#btn id=btnResend class="btn btn-default">Send all</a> ';
 $htm.='<a href=#btn id=btnDownload class="btn btn-default">Download</a> ';
 $htm.='</div>';
