fb7e66c879dede8cce773fdc89782dd24bdbde03
orange <jambonbill@gmail.com>
2017-05-01 00:10:25 +0800

    <div class="btn-group pull-right">

--- web/opafm/box_actions.php
+++ web/opafm/box_actions.php
@@ -3,12 +3,16 @@
 $htm='<div class="row">';
 $htm.='<div class="col-md-12">';
 $htm.='<a href=#btn id=btnRandom class="btn btn-default">Random patch</a> ';
-$htm.='<a href=#btn id=btnLoad class="btn btn-default">load Internal</a> ';
-$htm.='<a href=#btn id=btnStore class="btn btn-default">store Internal</a> ';
-$htm.='<a href=#btn id=btnKill1 class="btn btn-default">all notes off</a> ';
-$htm.='<a href=#btn id=btnKill2 class="btn btn-default">all sounds off</a> ';
 $htm.='<a href=#btn id=btnResend class="btn btn-default">Send all</a> ';
 $htm.='<a href=#btn id=btnDownload class="btn btn-default">Download</a> ';
+
+$htm.='<div class="btn-group pull-right">';
+$htm.='<a href=#btn id=btnLoad class="btn btn-default">Load Internal</a> ';
+$htm.='<a href=#btn id=btnStore class="btn btn-default">Store Internal</a> ';
+$htm.='<a href=#btn id=btnKill1 class="btn btn-default">All notes off</a> ';
+$htm.='<a href=#btn id=btnKill2 class="btn btn-default">All sounds off</a> ';
+$htm.='</div>';
+
 $htm.='</div>';
 $htm.='</div>';
 
