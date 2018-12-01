7510394c275ee1070ee7d4ff79e498d3828670c8
jambonbill <jambonbill@gmail.com>
2018-12-01 14:16:12 +0800

    <div class='col-sm-12'>

--- web/keyboard/midi_channels.php
+++ web/keyboard/midi_channels.php
@@ -1,30 +1,37 @@
 
 <div class='row'>
-	<div class='col-sm-12'>
+	
+    <div class='col-sm-12'>
+    
     <nav aria-label="Midi channel">
+      
       <ul class="pagination">
-        <li>
+      
+        <li class="page-item">
+      
           <a href="#" aria-label="Previous">
             Midi channel:
           </a>
         </li>
-        <li class='active' data-channel=0><a href=#>1</a></li>
-        <li data-channel=1><a href=#>2</a></li>
-        <li data-channel=2><a href=#>3</a></li>
-        <li data-channel=3><a href=#>4</a></li>
-        <li data-channel=4><a href=#>5</a></li>
-        <li data-channel=5><a href=#>6</a></li>
-        <li data-channel=6><a href=#>7</a></li>
-        <li data-channel=7><a href=#>8</a></li>
-        <li data-channel=8><a href=#>9</a></li>
-        <li data-channel=9><a href=#>10</a></li>
-        <li data-channel=10><a href=#>11</a></li>
-        <li data-channel=11><a href=#>12</a></li>
-        <li data-channel=12><a href=#>13</a></li>
-        <li data-channel=13><a href=#>14</a></li>
-        <li data-channel=14><a href=#>15</a></li>
-        <li data-channel=15><a href=#>16</a></li>
+        
+        <li class='page-item active' data-channel=0><a href=#>1</a></li>
+        <li class='page-item' data-channel=1><a href=#>2</a></li>
+        <li class='page-item' data-channel=2><a href=#>3</a></li>
+        <li class='page-item' data-channel=3><a href=#>4</a></li>
+        <li class='page-item' data-channel=4><a href=#>5</a></li>
+        <li class='page-item' data-channel=5><a href=#>6</a></li>
+        <li class='page-item' data-channel=6><a href=#>7</a></li>
+        <li class='page-item' data-channel=7><a href=#>8</a></li>
+        <li class='page-item' data-channel=8><a href=#>9</a></li>
+        <li class='page-item' data-channel=9><a href=#>10</a></li>
+        <li class='page-item' data-channel=10><a href=#>11</a></li>
+        <li class='page-item' data-channel=11><a href=#>12</a></li>
+        <li class='page-item' data-channel=12><a href=#>13</a></li>
+        <li class='page-item' data-channel=13><a href=#>14</a></li>
+        <li class='page-item' data-channel=14><a href=#>15</a></li>
+        <li class='page-item' data-channel=15><a href=#>16</a></li>
       </ul>
+
     </nav>
 </div>
 </div>
