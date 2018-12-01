444b6628afbce05f1db9a0eab72a8625507bdb59
jambonbill <jambonbill@gmail.com>
2018-12-01 14:15:33 +0800

    +	"description":"Online MIDI Tools by jambonbill",

--- web/home/meta.php
+++ web/home/meta.php
@@ -21,3 +21,20 @@ $META[]=['name'=>"description",   'content'=>'MIDI tools and stuff'];
 
 //
 $admin->meta($META);//Set Meta's
+
+/*
+<link rel="canonical" href="https://www.petcorp.org/" />
+<meta property="og:title" content="PET.CORP" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://www.petcorp.org/" />
+<meta property="og:image" content="https://www.petcorp.org/wp-content/uploads/2018/01/cropped-PETCORP_A4_A-01.png" />
+<meta property="og:site_name" content="PET.CORP" />
+<meta property="og:description" content="The PETSCII Corporation" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@pet_corp" />
+<meta name="twitter:domain" content="www.petcorp.org" />
+<meta name="twitter:title" content="PET.CORP" />
+<meta name="twitter:description" content="The PETSCII Corporation" />
+<meta name="twitter:image" content="https://www.petcorp.org/wp-content/uploads/2018/01/cropped-PETCORP_A4_A-01.png" />
+<meta itemprop="image" content="https://www.petcorp.org/wp-content/uploads/2018/01/cropped-PETCORP_A4_A-01.png" />
+ */
